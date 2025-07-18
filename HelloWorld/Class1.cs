a029947d3145f213c66f18d5e752cd9f8b07e4ab
rineee <rinemarlinac@gmail.com>
2025-07-18 15:46:05 +0700

    Add files via upload

---
+++ HelloWorld/Class1.cs
@@ -0,0 +1,17 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+
+namespace HelloWorld
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Class1 : IExternalCommand
+    {
+        public Autodesk.Revit.UI.Result Execute(ExternalCommandData revit,
+            ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("Revit", "HelloWorld");
+            return Autodesk.Revit.UI.Result.Succeeded;
+        }
+    }
+}
